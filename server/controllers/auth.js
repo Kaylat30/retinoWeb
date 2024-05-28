7b0d97163472368810e1f738f8bdbd8c8a26c23f
Kaylat30 <kayondoabdulatif@gmail.com>
2024-05-28 11:23:33 +0300

    http test1

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -51,7 +51,8 @@ export const login = (req, res, next) => {
       res.cookie('firstname', JSON.stringify({firstname:user.firstname}), {
         maxAge: 60000, 
         secure: true,
-        sameSite: 'none'  
+        sameSite: 'none',
+        httpOnly: false
       }); 
          
         res.status(200).json({success : true , message: "logged in successfully" , firstName: user.firstname });
